0715dfd89a375e551968508579d1b57bbfe800cc
Beau McCartney <mccartney.beausl@gmail.com>
2021-10-18 17:52:29 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * ENCM Driver Project 2
+ * Author: beaum, apo, quentin
+ *
+ * Created on October 18, 2021, 5:47 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
