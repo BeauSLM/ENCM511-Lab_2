d8100c70159ad802760d7c6d8bce07e4b5bfa0ef
Michael Corleone <32565641+MichaelDonCorleone@users.noreply.github.com>
2021-10-20 03:03:28 -0600

    Second Draft

--- main.c
+++ main.c
@@ -16,49 +16,40 @@
 int CNFlag = 0;
 //int TMR2Flag = 0;
 
-void NewClk(unsigned int clkval)  
+//sets the clock to 500kHz
+void CLKinit()  
 {
-    char COSCNOSC;
-    if (clkval == 8)  //8MHz
-    {
-        COSCNOSC = 0x00;
-    }
-    else if (clkval == 500) // 500 kHz
-    {
-        COSCNOSC = 0x66;
-    }
-    else if (clkval== 32) //32 kHz
-    {
-        COSCNOSC = 0x55; 
-    }
-    else // default 32 kHz
-    {
-        COSCNOSC = 0x55;
-    }
-     // Switch clock to 500 kHz
-     SRbits.IPL = 7;  //Disable interrupts
+     SRbits.IPL = 7;  //Disable interrupts before switch
      CLKDIVbits.RCDIV = 0;  // CLK division = 0
-     __builtin_write_OSCCONH(COSCNOSC);   // (0x00) for 8MHz; (0x66) for 500kHz; (0x55) for 32kHz;
-     __builtin_write_OSCCONL(0x01);
-     OSCCONbits.OSWEN=1;
-     while(OSCCONbits.OSWEN==1)
-     {} 
-     SRbits.IPL = 0;  //enable interrupts
+	 //prepares a 32kHz clock switch
+     __builtin_write_OSCCONH(0x55); //writes to upper half of OSCCON
+     __builtin_write_OSCCONL(0x01); //writes to lower half of OSCCON
+     OSCCONbits.OSWEN=1; //indicates a clock switch
+     while(OSCCONbits.OSWEN==1) {} //waits until the bit switches back
+	 //indicates a successful clock switch
+     SRbits.IPL = 0;  //Enable interrupts after switch
 }
+
 /*
  * Function void IOinit() sets up our input and output pin
  * behavior for the program we are building.
  */
 void IOinit() {
     TRISBbits.TRISB8 = 0; //SET GPIO RB8 as a digital output.
-    TRISAbits.TRISA2 = 0; //SET GPIO RA2 as a digital input.
+	
+    TRISAbits.TRISA2 = 1; //SET GPIO RA2 as a digital input.
     TRISAbits.TRISA4 = 1; //SET GPIO RA4 as a digital input.
     TRISBbits.TRISB4 = 1; //SET GPIO RB4 as a digital input.
+	
     CNPU2bits.CN30PUE = 1; //Enables Pull up resistor on RA2/ CN30
     CNPU1bits.CN0PUE = 1; //Enables Pull up resistor on RA4/ CN0
     CNPU1bits.CN1PUE = 1; //Enables Pull resistor on RB4 / CN1
+	
     AD1PCFG = 0xFFFF; // Turn all analog pins as digital
-    NewClk(32);
+	
+    IPC4bits.CNIP = 4; //sets priority for Input Change Notification
+	IFS1bits.CNIF = 0; //Clear the CNI flag
+    IEC1bits.CNIE = 1; //Input Change Notification Interrupts Enabled
 }
 
 
@@ -76,8 +67,9 @@ void Delay_ms(unsigned int time_ms) {
     
     //Interrupt Configuration for Timer 2
     IPC1bits.T2IP = 3; //Set Priority level = 3
-    IEC0bits.T2IE = 1; //Timer 2 interrupt enabled
     IFS0bits.T2IF = 0; //Clear Timer 2 Flag
+    IEC0bits.T2IE = 1; //Timer 2 interrupt enabled
+
     
     PR2 = 1000*32*time_ms/16; //Number of clock cycles that need to elapse
     T2CONbits.TON = 1; //Timer 2 Starts here
@@ -129,6 +121,7 @@ void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void) {
 
 int main(void) {
     IOinit();
+	CLKinit();
     while(1) {
         
     }
