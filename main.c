43eda8e1554f97248abc679a691d17306aabbe7f
Michael Corleone <32565641+MichaelDonCorleone@users.noreply.github.com>
2021-10-20 00:55:34 -0600

    First Complete Draft

--- main.c
+++ main.c
@@ -10,43 +10,12 @@
 #include "xc.h"
 #include <p24F16KA101.h>
 
-/*
- * Function void IOinit() sets up our input and output pin
- * behavior for the program we are building.
- */
 
+
+//Global Variables
 int CNFlag = 0;
-int TMR2Flag = 0;
-//clkval = 8 for 8MHz; 
-//clkval = 500 for 500kHz; 
-//clkval = 32 for 32kHz; 
-void Delay_ms(unsigned int time_ms) {
-    //Configuring T2CON register
-    T2CONbits.T32 = 0; //Operate as 16 bit timer
-    T2CONbits.TCS = 0; // Set internal clock usage
-    
-    //Prescaler settings of T2CON register
-    T2CONbits.TCKPS0 = 0; //
-    T2CONbits.TCKPS1 = 1; // These two lines set prescaling to 8x
-    
-    //Interrupt Configuration for Timer 2
-    //IPC1bits.T2IP = 7; //Set Priority level = 3
-    IEC0bits.T2IE = 1; //Timer 2 interrupt enabled
-    IFS0bits.T2IF = 0; //Clear Timer 2 Flag
-    
-    PR2 = 1000*32*time_ms/16; //Number of clock cycles that need to elapse
-    T2CONbits.TON = 1; //starts timer
-    return;
-    
-}
+//int TMR2Flag = 0;
 
-void Toggle_LED() {
-    if(PORTBbits.RB8 == 0) {
-        LATBbits.LATB8 = 1;
-    } else {
-        LATBbits.LATB8 = 0;
-    }
-}
 void NewClk(unsigned int clkval)  
 {
     char COSCNOSC;
@@ -76,6 +45,10 @@ void NewClk(unsigned int clkval)
      {} 
      SRbits.IPL = 0;  //enable interrupts
 }
+/*
+ * Function void IOinit() sets up our input and output pin
+ * behavior for the program we are building.
+ */
 void IOinit() {
     TRISBbits.TRISB8 = 0; //SET GPIO RB8 as a digital output.
     TRISAbits.TRISA2 = 0; //SET GPIO RA2 as a digital input.
@@ -87,21 +60,31 @@ void IOinit() {
     AD1PCFG = 0xFFFF; // Turn all analog pins as digital
     NewClk(32);
 }
- void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void)
- {
-    IFS1bits.CNIF = 0;
-    CNFlag = 1;
-    IOCheck();
- }
 
-void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void) {
-    IFS0bits.T2IF = 0; //Clear timer 2 interrupt flag
-    T2CONbits.TON = 0; //Stop timer
-    Toggle_LED();
-    TMR2 = 0;
-    TMR2Flag = 1; //Timer complete
-} 
-void IOCheck() {
+
+//clkval = 8 for 8MHz; 
+//clkval = 500 for 500kHz; 
+//clkval = 32 for 32kHz; 
+void Delay_ms(unsigned int time_ms) {
+    //Configuring T2CON register
+    T2CONbits.T32 = 0; //Operate as 16 bit timer
+    T2CONbits.TCS = 0; // Set internal clock usage
+    
+    //Prescaler settings of T2CON register
+    T2CONbits.TCKPS0 = 0; //
+    T2CONbits.TCKPS1 = 1; // These two lines set prescaling to 8x
+    
+    //Interrupt Configuration for Timer 2
+    IPC1bits.T2IP = 3; //Set Priority level = 3
+    IEC0bits.T2IE = 1; //Timer 2 interrupt enabled
+    IFS0bits.T2IF = 0; //Clear Timer 2 Flag
+    
+    PR2 = 1000*32*time_ms/16; //Number of clock cycles that need to elapse
+    T2CONbits.TON = 1; //Timer 2 Starts here
+    return;
+    
+}
+void IOcheck() {
     if(CNFlag == 1) {
         if(PORTAbits.RA2 == 1 && PORTAbits.RA4 == 1 && PORTBbits.RB4 == 1) {
             LATBbits.LATB8 = 0; //turn LED off in case no button is pressed
@@ -117,13 +100,34 @@ void IOCheck() {
         } else { 
             LATBbits.LATB8 = 1; //turn LED on if multiple buttons are pressed
         }
-        CNFlag = 0;
+        CNFlag = 0; // Set our CN Global Flag to False after we handle the interrupt
+    }
+}
+void Toggle_LED() {
+    if(PORTBbits.RB8 == 0) {
+        LATBbits.LATB8 = 1;
+    } else {
+        LATBbits.LATB8 = 0;
     }
 }
 
+ void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void)
+ {
+    IFS1bits.CNIF = 0; //Clear interrupt Register Flag
+    CNFlag = 1; // Set our CN Global Flag to True/1
+    IOcheck();
+ }
+
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void) {
+    IFS0bits.T2IF = 0; //Clear timer 2 interrupt flag
+    T2CONbits.TON = 0; //Stop timer
+    Toggle_LED();
+    TMR2 = 0; //Reset the TMR2 register after the interrupt occurs.
+    //TMR2Flag = 1; //Timer complete
+} 
+
+
 int main(void) {
-
-
     IOinit();
     while(1) {
         
